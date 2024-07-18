a771f3f565ff205bc37aa830b602e8faece9a941
ABCganada <shinmg9712@gmail.com>
2024-07-18 19:02:16 +0900

    feat: Url filter 구현

---
+++ src/main/java/kr/co/polycube/backendtest/filter/UrlFilter.java
@@ -0,0 +1,40 @@
+package kr.co.polycube.backendtest.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+@Component
+public class UrlFilter extends OncePerRequestFilter {
+
+    private static final Pattern pattern = Pattern.compile("[^a-zA-Z0-9/?&=:]");
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String url = request.getRequestURI() + request.getQueryString();
+        System.out.println(url);
+
+        //db 접속 위해 통과
+        if (url.startsWith("/h2-console")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        if (pattern.matcher(url).find()) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write("{\"reason\": \"유효하지 않은 URL\"}");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
